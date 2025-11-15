1085db4638a3138eba4c2c64a866d828157e650d
Or Gershoni <orgersh@gmail.com>
2025-11-16 01:50:19 +0200

    phase 1: ignore migrations on ruff scan

--- src/backend/InvenTree/build/test_build.py
+++ src/backend/InvenTree/build/test_build.py
@@ -231,7 +231,7 @@ class BuildTest(BuildTestBase):
         # Set build reference back to default value
         set_global_setting(
             'BUILDORDER_REFERENCE_PATTERN',
-            'BO-{ref:04d}',  # noqa: RUF027
+            'BO-{ref:04d}',
             change_user=None,
         )
 
@@ -254,7 +254,7 @@ class BuildTest(BuildTestBase):
             )
 
         # Try a new validator pattern
-        set_global_setting('BUILDORDER_REFERENCE_PATTERN', '{ref}-BO', change_user=None)  # noqa: RUF027
+        set_global_setting('BUILDORDER_REFERENCE_PATTERN', '{ref}-BO', change_user=None)
 
         for ref in ['1234-BO', '9999-BO']:
             Build.objects.create(
@@ -264,7 +264,7 @@ class BuildTest(BuildTestBase):
         # Set build reference back to default value
         set_global_setting(
             'BUILDORDER_REFERENCE_PATTERN',
-            'BO-{ref:04d}',  # noqa: RUF027
+            'BO-{ref:04d}',
             change_user=None,
         )
 
